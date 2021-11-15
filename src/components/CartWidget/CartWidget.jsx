c404f8cd0c3ccbf69aedaba9f0a02814567874c9
FacuDambo <dambolenafacundo@gmail.com>
2021-11-15 20:32:04 -0300

    Se agregaron cambios faltantes

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import {RiShoppingCart2Line} from 'react-icons/ri'
+import { useCartContext } from '../../context/CartContext'
+
 
 export default function CartWidget() {
+    const {itemCantidad} = useCartContext ()
     return (
-        <RiShoppingCart2Line className="cart-widget" />
+        <div>
+            <RiShoppingCart2Line className="cart-widget" />
+            <span className={
+                itemCantidad > 0
+                ? "carrito-badge"
+                : "carrito-none"
+            }>{itemCantidad}</span>
+        </div>
+        
     )
 }
